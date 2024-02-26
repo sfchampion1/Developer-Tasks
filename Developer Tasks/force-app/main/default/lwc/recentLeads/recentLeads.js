f6c2751347a58d4442f581116b3518106ef57d73
sfchampion1 <aamirabbas2021@hotmail.com>
2024-02-27 00:24:22 +0500

    Add wire and track imports, define columns in datatable, and call Apex method to retrieve recent leads

--- Developer Tasks/force-app/main/default/lwc/recentLeads/recentLeads.js
+++ Developer Tasks/force-app/main/default/lwc/recentLeads/recentLeads.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import getRecentLeads from '@salesforce/apex/LeadController.getRecentLeads';
+// Define the columns in the datatable
+const COLUMNS = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Company', fieldName: 'Company' },
+    { label: 'Status', fieldName: 'Status' },
+];
+// Export the class to make it available in the template
+export default class RecentLeads extends LightningElement {
+    @track error;
+    @track leads;
+    columns = COLUMNS;
 
-export default class RecentLeads extends LightningElement {}
+    // Call the Apex method to retrieve the recent leads
+    @wire(getRecentLeads)
+    wiredLeads({ error, data }) {
+        if (data) {
+            this.leads = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.leads = undefined;
+        }
+    }
+}
